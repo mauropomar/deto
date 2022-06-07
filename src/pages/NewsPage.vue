715c2eb5cfff264bff0b07e6ab62193df99de81e
mauropomar <mauropomardurruthy@gmail.com>
2022-06-07 08:17:30 -0700

    Configure the carousel of images for the news page

--- src/pages/NewsPage.vue
+++ src/pages/NewsPage.vue
@@ -1,13 +1,32 @@
 <template>
-  <q-page class="flex flex-center">
-      Noticias
+  <q-page>
+     <div class="q-pa-md">
+    <q-carousel
+      swipeable
+      animated
+      v-model="slide"
+      thumbnails
+      infinite
+      height="80vh"
+    >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg"/>
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    </q-carousel>
+  </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import { ref } from 'vue'
 
 export default defineComponent({
-  name: 'NewsPage'
+  name: 'NewsPage',
+  setup () {
+    return {
+      slide: ref(1)
+    }
+  }
 })
 </script>
