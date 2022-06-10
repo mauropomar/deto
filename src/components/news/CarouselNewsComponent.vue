c4e2b52a5401c200c39b4315f3d2df5152bbbee4
mauropomar <mauropomardurruthy@gmail.com>
2022-06-10 01:07:06 -0700

    Reuse components to optimize the system
    Create the carousel components to manage the news

---
+++ src/components/news/CarouselNewsComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-carousel
+    swipeable
+    animated
+    v-model="slide"
+    thumbnails
+    infinite
+    height="80vh"
+  >
+    <q-carousel-slide
+      :name="1"
+      img-src="https://cdn.quasar.dev/img/mountains.jpg"
+    />
+    <q-carousel-slide
+      :name="2"
+      img-src="https://cdn.quasar.dev/img/parallax1.jpg"
+    />
+    <q-carousel-slide
+      :name="3"
+      img-src="https://cdn.quasar.dev/img/parallax2.jpg"
+    />
+  </q-carousel>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "CarouselNewsComponent",
+  setup() {
+    return {
+      slide: ref(1),
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/pages/NewsPage.vue
+++ src/pages/NewsPage.vue
@@ -1,32 +1,19 @@
 <template>
   <q-page>
      <div class="q-pa-md">
-    <q-carousel
-      swipeable
-      animated
-      v-model="slide"
-      thumbnails
-      infinite
-      height="80vh"
-    >
-      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg"/>
-      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
-      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
-    </q-carousel>
+       <app-carousel-news></app-carousel-news>
   </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import { ref } from 'vue'
+import CarouselNewsComponent from "components/news/CarouselNewsComponent";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: 'NewsPage',
-  setup () {
-    return {
-      slide: ref(1)
-    }
+  components:{
+    'app-carousel-news': CarouselNewsComponent
   }
 })
 </script>
